e86566b5c9b15350b46ad22aa69169db75fc28cb
Kong Mong Vang <vangkong@icloud.com>
2025-06-19 16:38:13 -0400

    added more examples

---
+++ J1Problem/J1Problem/Controllers/J1Q2.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace J1Problem.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class J1Q2 : ControllerBase
+{
+    
+}
